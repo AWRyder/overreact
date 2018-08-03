eae48a4d88664aea56216810bc601522c37c9a28
João Braz <joao.braz@b-i.com>
2018-08-03 16:54:23 +0100

    initial

---
+++ backend/src/main/java/net/wyvernia/overreact/OverreactApplication.java
@@ -0,0 +1,12 @@
+package net.wyvernia.overreact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OverreactApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OverreactApplication.class, args);
+    }
+}
---
+++ backend/src/test/java/net/wyvernia/overreact/OverreactApplicationTests.java
@@ -0,0 +1,16 @@
+package net.wyvernia.overreact;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OverreactApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
